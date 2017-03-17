f2e3c53d789fa5648ef5ef009712b8c10fb7778a
xpavlov <konstantin.pavlov@phystech.edu>
2017-03-17 13:24:39 +0300

    Style fix

--- src/main/java/track/lessons/lesson3/Stack.java
+++ src/main/java/track/lessons/lesson3/Stack.java
@@ -3,5 +3,6 @@ package track.lessons.lesson3;
 // Стек - структура данных, удовлетворяющая правилу Last IN First OUT
 interface Stack {
     void push(int value); // положить значение наверх стека
+
     int pop(); // вытащить верхнее значение со стека
 }
